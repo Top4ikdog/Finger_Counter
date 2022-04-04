7ce5d6d2b0b5b9b7882bfb25dc4d396435a913af
Paha228 <BorovikPI@yandex.ru>
2022-04-04 16:41:11 +0700

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,3 +1,4 @@
+from sre_constants import SUCCESS
 import cv2
 import mediapipe as mp
 
@@ -5,4 +6,21 @@ cap = cv2.VideoCapture(0)    #подключение камеры
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands = 1)
-mpVisual = mp.solutions.draw
+mpVisual = mp.solutions.drawing_utils
+fing_Code = [(8 ,6), (12,10), (16,14), (20,18)]
+thumb_coord = (4,2)
+
+while cap.isOpened():
+    succes, image = cap.read()
+    if not succes:
+        print("Не удалось подкглючить камеру")
+        continue
+    image = cv2.flip(image, 1)
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result = hands.process(rgb_image)
+    multiLandMarks = result.multi_hand_landmarks
+    
+    if multiLandMarks:
+        for idx, handLms, in enumerate(multiLandMarks):
+            inl = result.multi_handedness[idx].classification[0].label
+            print(inl)
