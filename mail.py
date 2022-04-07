62f53f66ef06be4ad8b4d58363c30c29721d3ad3
Paha228 <BorovikPI@yandex.ru>
2022-04-07 15:34:57 +0700

    Update mail.py

--- mail.py
+++ mail.py
@@ -39,7 +39,7 @@ while cap.isOpened():
             print(inl)
         upCount = 0
         for Handing in multiLandMarks: 
-            mpVisual.draw_landmarks(image, Handing, mp_hands.Hand_CONNECTIONS)
+            mpVisual.draw_landmarks(image, Handing, mp_hands.Hand_Connections)
             list = []
             for id, lane in enumerate(handLms.landmark):
                 he, wi, l = image.shape
